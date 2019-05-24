afe658a04d4395319ced1a0881b5e7c2d0ea768d
Alex Moreno <alx.spain@gmail.com>
2019-05-24 20:23:46 +0200

    adding functions to help and error_msg

--- copy.c
+++ copy.c
@@ -11,33 +11,44 @@
 #define INFILE 2
 #define OUTFILE 3
 
+
+int help(char const *name) {
+  printf("usage:\n"
+  " %s  [-h|--help] [-m] <file_name> <new_file_name>\n"
+  "\n"
+  "   Copy files using read() or write() or not (-m to use them)\n"
+  "   if not, it will use mmap() and memcpy()\n"
+  "\n"
+  " %s correct syntax examples:\n"
+  "   %s --help\n"
+  "   %s -m filename_input.txt filename_output.txt\n"
+  "   %s filename_input.txt filename_output.txt\n"
+  " %s incorrect syntax examples:\n"
+  "   %s --d\n", name, name, name, name, name, name, name);
+  return 0;
+}
+
+int error_msg(char *message, char const *name, int error_code) {
+  printf("%s, see help: %s -h\n", message, name);
+  return error_code;
+}
+
 int main(int argc, char const *argv[]) {
   if (argc < 2) {
     /* Returns error code 1 which means no options given. */
-    printf("No options given, see help: %s -h\n", argv[NAME]);
-    return 1;
+    return error_msg("No options given", argv[NAME], 1);
   } else if (argc == 2) {
     if (strcmp(argv[OPTIONS],"-h") == 0 || strcmp(argv[OPTIONS],"--help") == 0) {
-      printf("usage:\n"
-      "%s  [-h|--help] [-m] <file_name> <new_file_name>\n"
-      "\n"
-      " Copy files using read() or write() or not (-m to use them)\n"
-      " if not, it will use mmap() and memcpy()\n"
-      "\n"
-      "%s correct syntax examples:\n"
-      "   %s --help\n"
-      "   %s -m filename_input.txt filename_output.txt\n"
-      "   %s filename_input.txt filename_output.txt\n"
-      "%s incorrect syntax examples:\n"
-      "   %s --d\n", argv[NAME], argv[NAME], argv[NAME], argv[NAME],
-      argv[NAME], argv[NAME], argv[NAME]);
-      return 0;
+      return help(argv[NAME]);
     } else {
       /* Returns error code 2 which menas bad option */
-      printf("Bad option, see help: %s -h\n", argv[NAME]);
-      return 2;
+      return error_msg("Bad option", argv[NAME], 2);
     }
   } else {
+    if (strcmp(argv[OPTIONS],"-h") == 0 || strcmp(argv[OPTIONS],"--help") == 0) {
+      /* Returns error code 2 which menas bad option */
+      return error_msg("Bad option", argv[NAME], 2);
+    }
     for (int i = 0; i < argc; i++) {
       printf("arguments: %s\n", argv[i]);
     }
